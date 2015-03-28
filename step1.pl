5f7a693a66c8ac77573af45368ed28770fce025e
jwalle <jwalle@localhost.localdomain>
2015-03-28 05:07:38 +0100

    tree

--- step1.pl
+++ step1.pl
@@ -1,5 +1,67 @@
-wizard(ron).
-hasWand(harry).
-qidditchPlayer(harry).
-wizard(X) :- hasBroom(X), hasWand(X).
-hasBroom(X) :- quidditchPlayer(X).
+% Commentaire
+
+femme(anne).
+femme(sylvie).
+femme(betty).
+femme(eve).
+femme(lisa).
+femme(julie).
+femme(valerie).
+
+homme(marc).
+homme(jean).
+homme(jules).
+homme(leon).
+homme(loic).
+homme(gerard).
+homme(jacques).
+homme(herve).
+homme(paul).
+homme(luc).
+
+mari_de(marc,anne).
+mari_de(luc,betty).
+mari_de(jules,lisa).
+mari_de(leon,sylvie).
+mari_de(loic,eve).
+mari_de(paul,julie).
+
+femme_de(anne,marc).
+femme_de(betty,luc).
+femme_de(lisa,jules).
+femme_de(sylvie,leon).
+femme_de(eve,loic).
+femme_de(julie,paul).
+
+enfant_de(jean,marc).
+enfant_de(jean,anne).
+enfant_de(jules,marc).
+enfant_de(jules,anne).
+enfant_de(leon,marc).
+enfant_de(leon,anne).
+
+enfant_de(lisa,luc).
+enfant_de(lisa,betty).
+enfant_de(loic,luc).
+enfant_de(loic,betty).
+enfant_de(gerard,luc).
+enfant_de(gerard,betty).
+
+enfant_de(herve,leon).
+enfant_de(herve,sylvie).
+enfant_de(julie,leon).
+enfant_de(julie,sylvie).
+
+enfant_de(jacques,jules).
+enfant_de(jacques,lisa).
+
+enfant_de(paul,loic).
+enfant_de(paul,eve).
+enfant_de(valerie,loic).
+enfant_de(valerie,eve).
+
+% beaupere_de(marc,lisa).
+
+% bellemere_de(anne,lisa).
+
+% ancetre_de().
